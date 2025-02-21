28c0747685f5a1c9af7b98325d2bde84e62237e7
bikramgiri <asgbrg.github@gmail.com>
2025-02-21 14:09:25 +0545

    Complete Project UI

--- src/pages/blog/components/card/Card.jsx
+++ src/pages/blog/components/card/Card.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Card = () => {
   return (
-      <div className="max-w-sm rounded overflow-hidden shadow-lg mb-4">
+    <Link to='/blog/id'>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg mb-4">
       <img className="w-full" src="https://emojical.net/wp-content/uploads/2023/02/man-mage-3.png" alt="Sunset in the mountains" />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -16,6 +18,7 @@ const Card = () => {
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
       </div>
     </div>
+    </Link>
   )
 }
 
