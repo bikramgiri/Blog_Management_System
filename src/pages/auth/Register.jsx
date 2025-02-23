76fd077698a92352e7c031989ba4b36cd78d5b2c
bikramgiri <asgbrg.github@gmail.com>
2025-02-23 13:23:10 +0545

    add register and login

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+// import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import Form from './components/Form/Form'
+import axios from 'axios'
 
 const Register = () => {
+  const navigate = useNavigate()
+  const handleRegister = async (data)=>{
+    try {
+      const response = await axios.post("https://react30.onrender.com/api/user/register",data)
+    if(response.status == 201){
+      navigate("/login")
+    }else{
+      alert("Registration failed")
+    }
+    } catch (error) {
+      alert(error?.response?.data?.message)
+    }
+  }
   return (
-	<Form type="Register"/>
+	<Form type="Register" onSubmit={handleRegister}/>
   )
 }
 
