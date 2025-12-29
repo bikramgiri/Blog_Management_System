b222fdff17edc491578ec77d79c6459d4a4fd57b
bikramgiri <asgbrg.github@gmail.com>
2025-12-29 11:23:48 +0545

    update navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
       
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<nav className="sticky top-0 z-50 bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
